426301f11137c77dad71c40a6f3b0edb67dd077e
sinardyas <sinar.dp@meteor.id>
2023-12-02 10:59:03 +0700

    chore: init commit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,36 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+import { PrismaClient } from "@prisma/client";
+
+const db = new PrismaClient();
+
+const app = new Elysia()
+  .get("/", () => "Hello Elysia")
+  .post(
+    "/register",
+    async ({ body }) => {
+      await db.user.create({ data: body });
+      return {
+        success: true,
+      };
+    },
+    {
+      error({ code }) {
+        switch (code) {
+          case "VALIDATION":
+            return { error: "Username must be unique" };
+
+          default:
+            break;
+        }
+      },
+      body: t.Object({
+        username: t.String(),
+        password: t.String({ minLength: 8 }),
+      }),
+    }
+  )
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
