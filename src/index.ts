28af400be833cb80e27f87f78073e13456d81639
sinardyas <sinar.dp@meteor.id>
2023-12-02 23:13:24 +0700

    feat: login api

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,30 @@
-import { Elysia, t } from "elysia";
+import { Elysia, t, NotFoundError } from "elysia";
+
+import { jwt } from "@elysiajs/jwt";
 
 import { PrismaClient } from "@prisma/client";
 
 const db = new PrismaClient();
 
 const app = new Elysia()
+  .use(
+    jwt({
+      name: "jwt",
+      secret: process.env.JWT_SECRET || "",
+      exp: process.env.JWT_EXPIRE || "",
+    })
+  )
   .get("/", () => "Hello Elysia")
   .post(
     "/register",
     async ({ body }) => {
-      await db.user.create({ data: body });
+      await db.user.create({
+        data: {
+          username: body.username,
+          password: Bun.password.hashSync(body.password),
+        },
+      });
+
       return {
         success: true,
       };
@@ -17,12 +32,46 @@ const app = new Elysia()
     {
       error({ code }) {
         switch (code) {
-          case "VALIDATION":
-            return { error: "Username must be unique" };
-
-          default:
-            break;
+          case "P2002":
+            return { code, error: "Username not available" };
         }
+      },
+      body: t.Object({
+        username: t.String(),
+        password: t.String({ minLength: 8 }),
+      }),
+    }
+  )
+  .post(
+    "/login",
+    async ({ body, jwt }) => {
+      const isUsernameExist = await db.user.findFirst({
+        where: { username: body.username },
+      });
+
+      if (!isUsernameExist) {
+        throw new Error("Invalid credentials");
+      }
+
+      const isPasswordMatch = await Bun.password.verify(
+        body.password,
+        isUsernameExist.password
+      );
+
+      if (!isPasswordMatch) {
+        throw new Error("Invalid credentials");
+      }
+
+      const token = await jwt.sign({
+        username: isUsernameExist.username,
+        iss: process.env.JWT_ISSUER || "",
+      });
+
+      return { token };
+    },
+    {
+      error({ code, error }) {
+        return { error: error.message };
       },
       body: t.Object({
         username: t.String(),
